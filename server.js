ed0a816fef1d90c93a8dfa28bee0a34aa7d2eb2e
marco19-sketch <marco.brusca1970@gmail.com>
2026-08-28 13:08:11 +0200

    Tried using cors, then commented it

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 import express from "express";
 import { inputCleaner, inputValidator } from "./middleware.js";
+// import cors from 'cors';
+
 const app = express();
+
+// app.use(cors({
+//     origin: 'http://127.0.0.1:5500'
+// }))
 
 app.use(express.urlencoded({ extended: true }));
 
